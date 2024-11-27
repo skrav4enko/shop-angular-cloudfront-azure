9479a29777a916823720144eead2c20bcccd1af9
Serhii Kravchenko <serhii_kravchenko1@epam.com>
2024-11-27 13:41:16 +0200

    feat: task-7 docker hello app

---
+++ libs/hello-world-app/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("*", (req, res) => {
+  const name = req.query.name || "World";
+  res.send(`Hello, ${process.env.CONTAINER_REGISTRY_NAME || "World"}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
